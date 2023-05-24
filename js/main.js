16f548d1cdd09e9307722a47043a6fc278711fd8
GaniraDennis <ganiradennis99@gmail.com>
2023-05-24 18:59:13 +0300

    fifth commit

--- js/main.js
+++ js/main.js
@@ -46,3 +46,66 @@ $(document).ready(function () {
     AOS.init();
 
 });
+
+$('#toggle').click(function() {
+    $(this).toggleClass('active');
+    $('#overlay').toggleClass('open');
+   });
+ $('.menu-cont').on('click', function() {
+   
+   var menu = $('.menu-txt').text();
+     var close = $('.menu-txt').attr('data-text');
+   
+   $('.menu').toggleClass('active');
+   
+   if ($('.menu-txt').text(menu)) {
+       $('.menu-txt').text(close);
+   } else {
+     $('.menu-txt').text(menu);
+   } 
+       $('.menu-txt').attr('data-text',menu);
+ })
+ var findNCenter = function() {
+   var elems = document.querySelectorAll('.center-vertical');
+ 
+   for(var i =0; i<elems.length; i++) {
+     elems[i].style.marginTop = (elems[i].parentNode.offsetHeight - elems[i].offsetHeight)/2 + 'px';
+   }
+ };
+ 
+ document.addEventListener && document.addEventListener('DOMContentLoaded', findNCenter);
+ window.addEventListener && window.addEventListener('resize', findNCenter);
+ 
+ $(document).ready(function() {
+ 
+   var count = $('#inner p').length;
+   var i = 1;
+     
+ setInterval(function() {
+   if (i < count) {
+     $('#inner').css('-webkit-transform','translate3d(0,-' + i + '00%,0)');
+       i++;
+   }
+ }, 800);
+ });
+
+ 
+ $(document).ready(function() {
+    var slides = $('.slide');
+    var slideIndex = 0;
+
+    function showSlide(index) {
+        slides.removeClass('active');
+        slides.eq(index).addClass('active');
+    }
+
+    function nextSlide() {
+        slideIndex++;
+        if (slideIndex >= slides.length) {
+            slideIndex = 0;
+        }
+        showSlide(slideIndex);
+    }
+
+    setInterval(nextSlide, 3000);
+});
